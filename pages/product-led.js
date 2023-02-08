68990b489cee421dd7564d3df609d52d22271307
Pavel Yarokhovich <pavel.yarokhovich@cactussoft.biz>
2023-02-08 10:25:23 +0300

    product led mock

--- pages/product-led.js
+++ pages/product-led.js
@@ -1,58 +1,76 @@
 import React, { useState } from "react";
-import Head from "next/head";
-import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 // Components
-import {
-  Heading,
-  Button,
-  Flex,
-  Container,
-  Text,
-  Input,
-} from "@chakra-ui/react";
+import { Button, Flex, Text, Input, Heading } from "@chakra-ui/react";
 
-const showcaseUrl =
-  "https://ds-9751-update-unit-list-card-layout-dot-prompto-showcase.appspot.com/ZDLLE?env=development";
-const showcaseIframeId = "showcase-iframe";
-
-export default function TestLocalization() {
+export default function ProductLed() {
   const [email, setEmail] = useState("");
+  const [source, setSource] = useState("");
   return (
     <div className={styles.container}>
+      <header>
+        <Heading as="h1" size="2xl" textAlign="center">
+          Mock Product Led
+        </Heading>
+      </header>
       <main className={styles.main}>
         <Flex
           width="100%"
+          maxW="500px"
           height="40px"
           marginBottom="15px"
           borderRadius="4px"
           alignItems="center"
         >
-          <Text whiteSpace="nowrap">Email</Text>
+          <Text whiteSpace="nowrap" w="120px">
+            Email
+          </Text>
           <Input
             variant="outline"
             value={email}
             onChange={(e) => {
               setEmail(e.target.value);
             }}
-            size="sm"
+            size="lg"
             marginLeft="15px"
           />
-          <Button
-            size="md"
-            marginLeft="10px"
-            onClick={() =>
-              window.open(
-                `http://ds-11079-story-add-new-register-form.vrtual-media-portal-dev.ew.r.appspot.com/product-led?email=${encodeURIComponent(
-                  email
-                )}`
-              )
-            }
-          >
-            Submit
-          </Button>
         </Flex>
+        <Flex
+          width="100%"
+          maxW="500px"
+          height="40px"
+          marginBottom="15px"
+          borderRadius="4px"
+          alignItems="center"
+        >
+          <Text whiteSpace="nowrap" w="120px">
+            Source
+          </Text>
+          <Input
+            variant="outline"
+            value={source}
+            onChange={(e) => {
+              setSource(e.target.value);
+            }}
+            size="lg"
+            marginLeft="15px"
+          />
+        </Flex>
+        <Button
+          size="md"
+          marginLeft="10px"
+          onClick={() =>
+            window.open(
+              `https://devapp.prompto.com/product-led?email=${encodeURIComponent(
+                email
+              )}&utm_source=${encodeURIComponent(source)}`,
+              "_blank"
+            )
+          }
+        >
+          Submit
+        </Button>
       </main>
     </div>
   );
